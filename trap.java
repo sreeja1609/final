c5fbf5611f98c1891b040a83bb80bb0a7659a21a
Admin <admin@Admins-MacBook-Pro-8.local>
2024-02-21 17:15:30 +0530

    final basic

---
+++ trap.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Trap{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a, b, h;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        h=sc.nextInt();
+    }
+}
